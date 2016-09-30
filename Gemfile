c35ecfe40d91d7f3e171f5dc23548158297a3026
nddquang <1312458@student.hcmus.edu.vn>
2016-10-01 01:07:50 +0700

    Add markdown using Redcarpet - function markdown in app/helpers/application_helper.rb

--- Gemfile
+++ Gemfile
@@ -14,6 +14,8 @@ gem 'bootstrap', '~> 4.0.0.alpha4'
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
+# Use redcarpet
+gem 'redcarpet'
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
 # See https://github.com/rails/execjs#readme for more supported runtimes
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    def markdown(text)
+        options = [:hard_wrap, :filter_html, :autolink, :no_intraemphasis]
+        markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
+                                            autolink: true,
+                                            hard_wrap:true,
+                                            filter_html:true,
+                                            no_intraemphasis: true)
+        markdown.render(text).html_safe
+    end
 end
